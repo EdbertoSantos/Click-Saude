3d2e9a9f9cd067e4e382e1e0183331cef54a949e
José Edberto Santos de França <edbertosantos.dev@gmail.com>
2023-03-22 02:51:22 -0300

    22/03 02:51 UPDATE

--- src/js/script_cadastro.js
+++ src/js/script_cadastro.js
@@ -19,56 +19,49 @@ form.addEventListener('submit', function (e) {
     const senha = String(inputSenha.value);
     const Confirmação_de_Senha = String(inputOksenha.value);
 
-    function alertaNAN(local) {
-        document.getElementsByName(local)[0].placeholder = 'Insira ' + local;
-        document.getElementById(local).style.border = "solid";
-        document.getElementById(local).style.borderColor = "Red";
-        document.getElementById(local).style.borderWidth = "2px";
-    }
-
-    function alertaOK(local) {
-        document.getElementById(local).style.border = "solid";
-        document.getElementById(local).style.borderColor = "rgba(0, 255, 110)";
-        document.getElementById(local).style.borderWidth = "2px";
-    }
-
-    function valorOk(valor1, valor2, local) {
-        if (valor1 == valor2) {
-            return alertaOK(local)
-        } else {
-            return alertaNAN(local)
-        }
-    }
-
-    if (!nome) {
-        alertaNAN('nome');
-    } else {
-        alertaOK('nome')
-    }
-
-    if (!email) {
-        alertaNAN('email');
-    } else {
-        alertaOK('email')
-    }
+    validacao(nome,'nome')
+    validacao(email,'email')
+    validacao(senha,'senha')
 
     if (!Confirmação_de_Email) {
         alertaNAN('Confirmação_de_Email');
     } else {
-        valorOk(email.value, Confirmação_de_Email.value, 'Confirmação_de_Email')
-    }
-
-    if (!senha) {
-        alertaNAN('senha');
-    } else {
-        alertaOK('senha')
+        senhasIguais(email, Confirmação_de_Email, 'Confirmação_de_Email');
     }
 
     if (!Confirmação_de_Senha) {
         alertaNAN('Confirmação_de_Senha');
     } else {
-        valorOk(senha.value, Confirmação_de_Senha.value, 'Confirmação_de_Senha')
+        senhasIguais(senha, Confirmação_de_Senha, 'Confirmação_de_Senha');
     }
 
 });
 
+function validacao(input,localizacao) {
+    if (!input) {
+        alertaNAN(localizacao);
+    } else {
+        alertaOK(localizacao);
+    }
+}
+
+function senhasIguais(local1, local2, local) {
+    if (local1 == local2) {
+        alertaOK(local)
+    } else {
+        alertaNAN(local)
+    }
+}
+
+function alertaNAN(local) {
+    document.getElementsByName(local)[0].placeholder = 'Insira ' + local;
+    document.getElementById(local).style.border = "solid";
+    document.getElementById(local).style.borderColor = "Red";
+    document.getElementById(local).style.borderWidth = "1px";
+}
+
+function alertaOK(local) {
+    document.getElementById(local).style.border = "solid";
+    document.getElementById(local).style.borderColor = "rgba(0, 255, 110)";
+    document.getElementById(local).style.borderWidth = "1px";
+}
